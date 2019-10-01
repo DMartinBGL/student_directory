0b340a053fb7a20d509390a121324e95d6258f42
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-01 16:33:02 +0100

    added comments

--- directory.rb
+++ directory.rb
@@ -11,6 +11,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#finally we put total number of students
 print "Overall, we have "
+#its important that print() doesn't add new line characters
 print "11"
 puts " great students"

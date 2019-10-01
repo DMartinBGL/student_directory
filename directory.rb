2961c2492df950dcfcd2fe9b0fccbdad07323535
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-01 17:14:06 +0100

    introduced methods

--- directory.rb
+++ directory.rb
@@ -13,8 +13,19 @@ students = [
 "Norman Bates"
 ]
 #finally we put total number of students
-puts "The Students of Villians Academy"
-puts "_____________"
-students.each do |student| puts student end
-puts "Overall, we have #{students.count} great students"
+def print_header
+    puts "The Students of Villians Academy"
+    puts "------------------------"
+end
 
+def print(names)
+    names.each do |name| puts name end
+end
+
+def print_footer(names)
+    puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)

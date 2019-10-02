9f3361b2932c092bf1022e81f6d437abeffd7f75
DMartinBGL <david.martin@bglgroup.co.uk>
2019-10-02 15:20:05 +0100

    end of task 7

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-#first, we print the list of students
-students = [
-  ["Dr. Hannibal Lecter", :november],
-  ["Darth Vader", :november],
-  ["Nurse Ratched", :november],
-  ["Michael Corleone", :november],
-  ["Alex DeLarge", :november],
-  ["The Wicked Witch of the West", :november],
-  ["Terminator", :november],
-  ["Freddy Krueger", :november],
-  ["The Joker", :november],
-  ["Joffrey Baratheon", :november],
-  ["Norman Bates", :november]
-]
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+
+    students = []
+
+    name = gets.chomp
+    name.capitalize!
+
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+
+        name = gets.chomp
+        name.capitalize!
+    end
+    students
+end
+
 #finally we put total number of students
 def print_header
     puts "The Students of Villians Academy"
@@ -19,13 +24,18 @@ def print_header
 end
 
 def print(students)
-    students.each do |student| puts "#{student[0]} (#{student[1]} cohort)" end
+    students.each do |student| 
+        puts "#{student[0]} (#{student[1]} cohort)" 
+    end
 end
 
 def print_footer(students)
     puts "Overall, we have #{students.count} great students"
 end
 
+
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
